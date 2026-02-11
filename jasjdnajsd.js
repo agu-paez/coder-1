9b4987b3368053dcca8691be491b7173f4a454f3
Agustin Paez <paezagustinnahuel@gmail.com>
2026-02-11 01:26:30 -0300

    primer commit

--- jasjdnajsd.js
+++ jasjdnajsd.js
@@ -1,8 +1,8 @@
 // Agus_Cardetail 
 let carrito = JSON.parse(localStorage.getItem("carrito_agus")) || [];
 
-let productos = JSON.parse(localStorage.getItem("productos_agus")) || [
-    {id:1 , nombre : "shampoo", precio: 20000, stock: 5, img: "static/img/shampoo.png",},
+const productos = [
+    {id:1 , nombre : "shampoo", precio: 20000, stock: 0, img: "static/img/shampoo.png",},
     {id:2 , nombre : "cera", precio: 10000, stock: 10, img: "static/img/cera.png",},
     {id:3 , nombre : "acondionador_ext", precio: 25000, stock: 1, img: "static/img/acondionador_ext.png"},
     {id:4 , nombre : "acondionador_int", precio: 50000, stock: 50, img: "static/img/acondionador_int.png"},
@@ -10,11 +10,6 @@ let productos = JSON.parse(localStorage.getItem("productos_agus")) || [
     {id:6 , nombre : "pulidora", precio: 50000, stock: 50, img: "static/img/pulidora.png"},
     {id:7 , nombre : "sellador", precio: 50000, stock: 50, img: "static/img/sellador.png"}
 ];
-
-const guardarLocal = () => {
-    localStorage.setItem("carrito_agus", JSON.stringify(carrito));
-    localStorage.setItem("productos_agus", JSON.stringify(productos));
-};
 
 const cargar_productos = () => {
     let contenedor = document.getElementById("cont_productos");
@@ -45,47 +40,29 @@ const cargar_productos = () => {
     let but_Agr = document.querySelectorAll(".btn_agregar"); 
     but_Agr.forEach((x) => x.addEventListener("click", (i) => { 
         let prod_enc = productos.find((p) => p.id == i.target.dataset.id); 
-        
+        //Control del stock 
         if (prod_enc.stock > 0) {
             let existe = carrito.find((p) => p.id == prod_enc.id);
 
             if (existe) {
                 existe.cantidad++; 
-                
             } else { 
                 prod_enc.cantidad = 1; 
                 carrito.push(prod_enc); 
             }
 
             prod_enc.stock--; 
-            
-            Toastify({
-                text: `¡${prod_enc.nombre.toUpperCase()} AGREGADO!`,
-                duration: 2000,
-                gravity: "top", 
-                position: "right",
-                className: "toast-personalizado", 
-                style: {
-                    background: "linear-gradient(to right, #004289, #0072ff)",
-                    borderRadius: "10px",
-                }
-            }).showToast();
-            
-            
-            guardarLocal();
             cargar_productos(); 
             total_Ul();
-
-            if (document.getElementById("ventana_carrito").style.display === "block") {
-                carrito_loading(); 
-            }
         } else {
             alert("¡No posee más stock de este producto!"); 
         }
-    }))
+    }));
+    //aca 
+    if (document.getElementById("ventana_carrito").style.display === "block") { //
+                carrito_loading(); 
+            }
 };
-
-
 
 const total_Ul = () => {
     const totalUnidades = carrito.reduce((acc, p) => acc + p.cantidad, 0);
@@ -95,65 +72,32 @@ const total_Ul = () => {
 const eliminarProducto = (id) => {
     let producto = carrito.find((p) => p.id === id);
     if (producto) {
-        
+        // enviamos al strock de donde lo sacamos 
         let prodOriginal = productos.find((p) => p.id === id);
         prodOriginal.stock++; 
-        Toastify({
-                    text: `¡${producto.nombre.toUpperCase()} ELIMINADO`,
-                    duration: 2000,
-                    gravity: "top", 
-                    position: "right",
-                    className: "toast-personalizado", 
-                    style: {
-                        background: "linear-gradient(to right, #7c019b, #ff0202)", 
-                        borderRadius: "10px",
-                    }
-                }).showToast();
-            
-        
-        
+
         if (producto.cantidad > 1) {
             producto.cantidad--; 
         } else {
             carrito = carrito.filter((p) => p.id !== id);
         }
     }
-    guardarLocal(); 
     total_Ul();
     cargar_productos();
     carrito_loading(); 
 };
 
 const limpiarCarrito = () => {
-    Swal.fire({
-        title: "¿Vaciar el carrito?",
-        text: "Esta acción eliminara todos los productos del carrito.",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#004289", 
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Sí, vaciarlo",
-        cancelButtonText: "Cancelar"
-        }).then((result) => {
-        if (result.isConfirmed) {
-            carrito.forEach(p => {
-                let prodOriginal = productos.find(po => po.id === p.id);
-                prodOriginal.stock += p.cantidad;
-            });
-            
-            carrito = []; 
-            localStorage.removeItem("carrito_agus"); 
-            total_Ul();
-            cargar_productos();
-            carrito_loading();
-            Swal.fire({title: "¡Vaciado!",
-                text: "El carrito se limpió correctamente.",
-                icon: "success",
-                timer: 1500,
-                showConfirmButton: false});
-        } 
+    // Devolvemos el stock antes de vaciar
+    carrito.forEach(p => {
+        let prodOriginal = productos.find(po => po.id === p.id);
+        prodOriginal.stock += p.cantidad;
     });
     
+    carrito = []; 
+    total_Ul();
+    cargar_productos();
+    carrito_loading();
 };
 
 const carrito_loading = () => {
@@ -173,6 +117,7 @@ const carrito_loading = () => {
         div_item.className = "lista_carrito";
         div_item.innerHTML = `
             <p>${p.nombre} | ${p.cantidad} | $${p.precio * p.cantidad} <button class="btn-quitar" data-id="${p.id}"> X </button></p>
+            
         `;
         lista_carrito.appendChild(div_item);
     });
@@ -188,90 +133,42 @@ const carrito_loading = () => {
     lista_carrito.appendChild(div_total);
 
     cont_ventana.style.display = "block";
-
+     
 };
 
-
 cargar_productos();
-total_Ul();
 
-document.getElementById("icon-carrito").addEventListener("click", (e) => {
-
-    const ventanaCarrito = document.getElementById("ventana_carrito");
-    
-    if (ventanaCarrito.style.display === "none" || ventanaCarrito.style.display === "") {
-        carrito_loading(); // 
-    } else {
-        ventanaCarrito.style.display = "none"; 
-    }
-});
- 
+document.getElementById("icon-carrito").addEventListener("click", carrito_loading); 
 
 
 document.getElementById("btn-cerrar").addEventListener("click", () => {
     document.getElementById("ventana_carrito").style.display = "none";
 });
-
-const actualizarInterfaz = () => {
-    total_Ul(); 
-    cargar_productos(); 
-    
-    
-    if (document.getElementById("ventana_carrito").style.display === "block") {
-        carrito_loading(); 
-    }
-};
-
-
 document.getElementById("btn-comprar").addEventListener("click", () => {
     if (carrito.length > 0) {
-     
-        Swal.fire({
-            title: 'Procesando tu compra',
-            text: 'Estamos verificando el stock...',
-            allowOutsideClick: false,
-            showConfirmButton: false, 
-            willOpen: () => {
-                Swal.showLoading(); 
-            }
-        });
-
-        try {
-    
-            setTimeout(() => {
-                
-                Swal.fire({
-                    icon: 'success',
-                    title: '¡Compra registrada!',
-                    text: 'Gracias por confiar en Agus_Cardetail',
-                    timer: 2000,
-                    showConfirmButton: false
-                });
-
-              
-                carrito = [];
-                localStorage.removeItem("carrito_agus");
-                actualizarInterfaz();
-                document.getElementById("ventana_carrito").style.display = "none";
-            }, 2000);
-
-        } catch (error) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error en el sistema',
-                text: 'No pudimos procesar la compra.',
-            });
-        }
+        
+            alert("Su compra fue registrada con exito!")
+            carrito = [];
+            document.getElementById("contador-carrito").innerText = "0";
+            document.getElementById("ventana_carrito").style.display = "none"; 
+            cargar_productos();  
+            
     } else {
-        Swal.fire({
-            icon: 'warning',
-            title: 'Carrito vacío',
-            text: 'Agregá algunos productos primero.',
-        });
+            alert("El carrito está vacío. Agregue productos para comprar.");
     }
 });
 
 
 
-cargar_productos();
-total_Ul();
+
+
+carrito.forEach(p => {
+        let prodOriginal = productos.find(po => po.id === p.id);
+        prodOriginal.stock += p.cantidad;
+    });
+    
+    carrito = []; 
+    localStorage.removeItem("carrito_agus"); 
+    total_Ul();
+    cargar_productos();
+    carrito_loading();
